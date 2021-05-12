32c475438d906db4f1e0881569e444099c46aea9
jlukenichols <J.Luke.Nichols@gmail.com>
2021-05-12 12:17:25 -0400

    Added error handling
    Added error handling for IP addresses on subnets that the server does not have a network interface on

--- WakeOnLANFromCSV.ps1
+++ WakeOnLANFromCSV.ps1
@@ -10,7 +10,7 @@
 
 .NOTES
     Release Date: 2021-04-30T11:48
-    Last Updated: 2021-05-12T11:56
+    Last Updated: 2021-05-12T12:14
    
     Author: Luke Nichols
     Github link: https://github.com/jlukenichols/WakeOnLANFromCSV
@@ -73,35 +73,23 @@ Write-Log $LogMessage
 $LogMessage = "`$FullPathToCSV = $FullPathToCSV"
 Write-Log $LogMessage
 
-# Define the appropriate IP Interface to send the WoL packets from
-#$AssociatedIPInterface = Get-NetIPInterface | Where-Object {$_.InterfaceAlias -like "$InterfaceAliasPattern"}
-# Determine the IP address of the interface to send the WoL packets from
-#$WoLInterface =  Get-NetIPAddress -AddressFamily IPv4 -AssociatedIPInterface $AssociatedIPInterface
-
-<#ForEach ($Interface in $WoLInterface) {
-    #Determine the CIDR prefix for the IP address and then convert to a subnet mask in decimal
-    #$BroadcastAddress = (Get-IPv4Subnet -IPAddress ($Interface.IPAddress) -Subnetmask (Convert-PrefixLengthToNetmask $($WoLInterface.PrefixLength))).Broadcast
-    $BroadcastAddress = (Get-IPv4Subnet -IPAddress ($Interface.IPAddress) -Subnetmask (CIDRToNetMask $($WoLInterface.PrefixLength))).Broadcast
-
-    $LogMessage = "`$BroadcastAddress = $BroadcastAddress"
-    Write-Log $LogMessage
-}#>
-
 #Import the CSV
 $ArrayOfMACAddresses = Import-Csv -Path $FullPathToCSV
 
-#Initialize a counter variable
-$count = 0
+#Initialize counter variables
+$PacketsSent = 0
+$UnwakeableCount = 0
 
-#Send a WoL packet to each MAC address in the CSV file
-:loopThroughCSVFile Foreach ($line in $ArrayOfMACAddresses) {
-    $count += 1
-    
+#Loop through the CSV file
+:loopThroughCSVFile Foreach ($line in $ArrayOfMACAddresses) {    
+    #Loop through the system's network interfaces
     :loopThroughSystemIPAddresses Foreach ($NetIPAddress in (Get-NetIPAddress | Where-Object {($_.AddressFamily -eq "IPv4") -and ($_.InterfaceAlias -notlike "*Loopback*")})) {
         $SubnetMask = CIDRToNetMask $NetIPAddress.PrefixLength
         $LogMessage = "Is $($line.IPAddress) in the same subnet as $($NetIPAddress.IPAddress)? Subnet: $SubnetMask"
         Write-Log $LogMessage
+        #Determine if the IP address of the current line in the CSV file is in the same subnet as the IP address of the current network interface
         if (Compare-Subnets $line.IPAddress $NetIPAddress.IPAddress $SubnetMask) {
+            #The IPs are in the same subnet, 
             $LogMessage = "Yes"
             Write-Log $LogMessage
 
@@ -114,17 +102,28 @@ $count = 0
             $LogMessage = "Sending WoL packet to $($line.ComputerName) with IP $($line.IPAddress) and MAC $($line.MACAddress)"
             Write-Log $LogMessage
             Invoke-WakeOnLan -Verbose -MacAddress $line.MACAddress -BroadcastAddress $BroadcastAddress
+            
+            #Increment the counter
+            $PacketsSent += 1
+
+            #Jump to the next line in the CSV file since we already found a matching NIC
             continue loopThroughCSVFile
         } else {
             $LogMessage = "No"
             Write-Log $LogMessage
-
-            continue loopThroughSystemIPAddresses
         }
     }
+    #If the script reaches this point then there is no network interface capable of waking this computer.
+    $LogMessage = "ERROR: There is no network interface on this system residing on the same subnet as IP $($line.IPAddress). Cannot Wake computer $($line.ComputerName)."
+    Write-Log $LogMessage
+
+    $UnwakeableCount += 1
 }
 
-$LogMessage = "Total Wake-on-LANs attempted: $count"
+$LogMessage = "Total Wake-on-LAN packets sent: $PacketsSent"
+Write-Log $LogMessage
+
+$LogMessage = "Unwakeable computers: $UnwakeableCount"
 Write-Log $LogMessage
 
 #-------------------------- End main script body --------------------------
